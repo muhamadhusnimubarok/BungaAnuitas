dd5c8cdaaf20226dc2d95d58e41145675d5c4a2d
Dynoxxx <muhamadhusnimubarok@smkwikrama.sch>
2024-10-25 11:21:23 +0700

    BungaAnuitass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,66 +1,148 @@
-@extends('layouts.app')
+<html>
+  <head>
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="" />
+    <link
+      rel="stylesheet"
+      as="style"
+      onload="this.rel='stylesheet'"
+      href="https://fonts.googleapis.com/css2?display=swap&amp;family=Noto+Sans%3Awght%40400%3B500%3B700%3B900&amp;family=Plus+Jakarta+Sans%3Awght%40400%3B500%3B700%3B800"
+    />
 
-@section('content')
-<div class="text-center mt-5">
-    <h1>Selamat Datang di Aplikasi Penghitung Bunga dan Anuitas</h1>
-    <p class="lead">Silahkan Pilih Kebutuhan Anda:</p>
-</div>
+    <title>Bunga Anuitas</title>
+    <link rel="icon" type="image/x-icon" href="data:image/x-icon;base64," />
 
-<div class="row mt-4">
-    <div class="col-md-3">
-        <div class="card h-100">
-            <!-- Ganti dengan URL gambar yang sesuai -->
-            <img src="{{ asset('img/tunggal.png') }}" class="card-img-top" alt="Simple Interest">
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">Kalkulator Menghitung Bunga Tunggal</h5>
-                <p class="card-text">Kalkulator bunga tunggal menghitung bunga berdasarkan nilai pokok, suku bunga, dan jangka waktu.</p>
-                <div class="mt-auto">
-                    <a href="/simple-interest-form" class="btn btn-primary">Go to Calculator</a>
-                </div>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
+  </head>
+  <body>
+    <div class="relative flex size-full min-h-screen flex-col bg-[#FFFFFF] group/design-root overflow-x-hidden" style='font-family: "Plus Jakarta Sans", "Noto Sans", sans-serif;'>
+      <div class="layout-container flex h-full grow flex-col">
+        <header class="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#F4EFE6] px-10 py-3">
+          <div class="flex items-center gap-4 text-[#1C160C]">
+            <div class="size-4">
+              <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M39.475 21.6262C40.358 21.4363 40.6863 21.5589 40.7581 21.5934C40.7876 21.655 40.8547 21.857 40.8082 22.3336C40.7408 23.0255 40.4502 24.0046 39.8572 25.2301C38.6799 27.6631 36.5085 30.6631 33.5858 33.5858C30.6631 36.5085 27.6632 38.6799 25.2301 39.8572C24.0046 40.4502 23.0255 40.7407 22.3336 40.8082C21.8571 40.8547 21.6551 40.7875 21.5934 40.7581C21.5589 40.6863 21.4363 40.358 21.6262 39.475C21.8562 38.4054 22.4689 36.9657 23.5038 35.2817C24.7575 33.2417 26.5497 30.9744 28.7621 28.762C30.9744 26.5497 33.2417 24.7574 35.2817 23.5037C36.9657 22.4689 38.4054 21.8562 39.475 21.6262ZM4.41189 29.2403L18.7597 43.5881C19.8813 44.7097 21.4027 44.9179 22.7217 44.7893C24.0585 44.659 25.5148 44.1631 26.9723 43.4579C29.9052 42.0387 33.2618 39.5667 36.4142 36.4142C39.5667 33.2618 42.0387 29.9052 43.4579 26.9723C44.1631 25.5148 44.659 24.0585 44.7893 22.7217C44.9179 21.4027 44.7097 19.8813 43.5881 18.7597L29.2403 4.41187C27.8527 3.02428 25.8765 3.02573 24.2861 3.36776C22.6081 3.72863 20.7334 4.58419 18.8396 5.74801C16.4978 7.18716 13.9881 9.18353 11.5858 11.5858C9.18354 13.988 7.18717 16.4978 5.74802 18.8396C4.58421 20.7334 3.72865 22.6081 3.36778 24.2861C3.02574 25.8765 3.02429 27.8527 4.41189 29.2403Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
             </div>
-        </div>
-    </div>
-
-    <div class="col-md-3">
-        <div class="card h-100">
-            <!-- Ganti dengan URL gambar yang sesuai -->
-            <img src="{{ asset('img/majemuk.png') }}" class="card-img-top" alt="Compound Interest">
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">Kalkulator Menghitung Bunga Majemuk</h5>
-                <p class="card-text">Kalkulator bunga majemuk menghitung bunga dengan mempertimbangkan nilai pokok, suku bunga, dan jangka waktu. Pengguna dapat memilih untuk menghitung nilai masa depan atau nilai awal di masa lalu.</p>
-                <div class="mt-auto">
-                    <a href="/compound-interest-form" class="btn btn-primary">Go to Calculator</a>
+            <h2 class="text-[#1C160C] text-lg font-bold leading-tight tracking-[-0.015em]">FINANSKU</h2>
+          </div>
+          <div class="flex gap-2">
+            <button
+              class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 bg-[#F4EFE6] text-[#1C160C] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5"
+            >
+              <div class="text-[#1C160C]" data-icon="List" data-size="20px" data-weight="regular">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                  <path
+                    d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+            <button
+              class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 bg-[#F4EFE6] text-[#1C160C] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5"
+            >
+              <div class="text-[#1C160C]" data-icon="Bell" data-size="20px" data-weight="regular">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                  <path
+                    d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216ZM48,184c7.7-13.24,16-43.92,16-80a64,64,0,1,1,128,0c0,36.05,8.28,66.73,16,80Z"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+            <button
+              class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 bg-[#F4EFE6] text-[#1C160C] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5"
+            >
+              <div class="text-[#1C160C]" data-icon="Gear" data-size="20px" data-weight="regular">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                  <path
+                    d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Zm88-29.84q.06-2.16,0-4.32l14.92-18.64a8,8,0,0,0,1.48-7.06,107.21,107.21,0,0,0-10.88-26.25,8,8,0,0,0-6-3.93l-23.72-2.64q-1.48-1.56-3-3L186,40.54a8,8,0,0,0-3.94-6,107.71,107.71,0,0,0-26.25-10.87,8,8,0,0,0-7.06,1.49L130.16,40Q128,40,125.84,40L107.2,25.11a8,8,0,0,0-7.06-1.48A107.6,107.6,0,0,0,73.89,34.51a8,8,0,0,0-3.93,6L67.32,64.27q-1.56,1.49-3,3L40.54,70a8,8,0,0,0-6,3.94,107.71,107.71,0,0,0-10.87,26.25,8,8,0,0,0,1.49,7.06L40,125.84Q40,128,40,130.16L25.11,148.8a8,8,0,0,0-1.48,7.06,107.21,107.21,0,0,0,10.88,26.25,8,8,0,0,0,6,3.93l23.72,2.64q1.49,1.56,3,3L70,215.46a8,8,0,0,0,3.94,6,107.71,107.71,0,0,0,26.25,10.87,8,8,0,0,0,7.06-1.49L125.84,216q2.16.06,4.32,0l18.64,14.92a8,8,0,0,0,7.06,1.48,107.21,107.21,0,0,0,26.25-10.88,8,8,0,0,0,3.93-6l2.64-23.72q1.56-1.48,3-3L215.46,186a8,8,0,0,0,6-3.94,107.71,107.71,0,0,0,10.87-26.25,8,8,0,0,0-1.49-7.06Zm-16.1-6.5a73.93,73.93,0,0,1,0,8.68,8,8,0,0,0,1.74,5.48l14.19,17.73a91.57,91.57,0,0,1-6.23,15L187,173.11a8,8,0,0,0-5.1,2.64,74.11,74.11,0,0,1-6.14,6.14,8,8,0,0,0-2.64,5.1l-2.51,22.58a91.32,91.32,0,0,1-15,6.23l-17.74-14.19a8,8,0,0,0-5-1.75h-.48a73.93,73.93,0,0,1-8.68,0,8,8,0,0,0-5.48,1.74L100.45,215.8a91.57,91.57,0,0,1-15-6.23L82.89,187a8,8,0,0,0-2.64-5.1,74.11,74.11,0,0,1-6.14-6.14,8,8,0,0,0-5.1-2.64L46.43,170.6a91.32,91.32,0,0,1-6.23-15l14.19-17.74a8,8,0,0,0,1.74-5.48,73.93,73.93,0,0,1,0-8.68,8,8,0,0,0-1.74-5.48L40.2,100.45a91.57,91.57,0,0,1,6.23-15L69,82.89a8,8,0,0,0,5.1-2.64,74.11,74.11,0,0,1,6.14-6.14A8,8,0,0,0,82.89,69L85.4,46.43a91.32,91.32,0,0,1,15-6.23l17.74,14.19a8,8,0,0,0,5.48,1.74,73.93,73.93,0,0,1,8.68,0,8,8,0,0,0,5.48-1.74L155.55,40.2a91.57,91.57,0,0,1,15,6.23L173.11,69a8,8,0,0,0,2.64,5.1,74.11,74.11,0,0,1,6.14,6.14,8,8,0,0,0,5.1,2.64l22.58,2.51a91.32,91.32,0,0,1,6.23,15l-14.19,17.74A8,8,0,0,0,199.87,123.66Z"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+          </div>
+        </header>
+        <div class="px-40 flex flex-1 justify-center py-5">
+          <div class="layout-content-container flex flex-col max-w-[960px] flex-1">
+            <a href="/comparison-form">
+            <div class="p-4">
+              <div class="flex items-stretch justify-between gap-4 rounded-xl bg-[#FFFFFF] p-4 shadow-[0_0_4px_rgba(0,0,0,0.1)]">
+                <div class="flex flex-col gap-1 flex-[2_2_0px]">
+                  <p class="text-[#1C160C] text-base font-bold leading-tight">PERBANDINGAN BUNGA TUNGGAL DAN MAJEMUK</p>
+                  <p class="text-[#A18249] text-sm font-normal leading-normal">27.09.2024 | 24:15:05</p>
                 </div>
+                <div
+                  class="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl flex-1"
+                  style='background-image: url("https://cdn.usegalileo.ai/sdxl10/739c61d0-f95e-401a-ad6d-cb9e6a743654.png");'
+                ></div>
+              </div>
             </div>
-        </div>
-    </div>
-
-    <div class="col-md-3">
-        <div class="card h-100">
-            <!-- Ganti dengan URL gambar yang sesuai -->
-            <img src="{{ asset('img/anuitas.jpg') }}" class="card-img-top" alt="Annuity">
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">Kalkulator Menghitung Anuitas</h5>
-                <p class="card-text">Kalkulator anuitas menghitung pembayaran berkala berdasarkan nilai pokok, suku bunga, dan jumlah periode. Ini membantu pengguna merencanakan pembayaran pinjaman atau investasi dengan cara yang terstruktur.</p>
-                <div class="mt-auto">
-                    <a href="/annuity-form" class="btn btn-primary">Go to Generator</a>
+            </a>
+            <h2 class="text-[#1C160C] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">MENU APLIKASI</h2>
+            <div class="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
+              <div class="flex flex-col gap-3 pb-3">
+                <a href="/simple-interest-form">
+                <div
+                  class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                  style='background-image: url("https://cdn.usegalileo.ai/sdxl10/a93f8313-e955-45a1-bf08-9da3398e3689.png");'
+                ></div>
+                <div>
+                  <p class="text-[#1C160C] text-base font-medium leading-normal">MENGHITUNG BUNGA TUNGGAL</p>
+                  <p class="text-[#A18249] text-sm font-normal leading-normal">Muhamad Husni Mubarok</p>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-3">
-        <div class="card h-100">
-            <!-- Ganti dengan URL gambar yang sesuai -->
-            <img src="{{ asset('img/comparison.jpg') }}" class="card-img-top" alt="Comparison">
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">Membandingkan Bunga Tunggak dan Majemuk</h5>
-                <p class="card-text">Kalkulator perbandingan ini menunjukkan perbedaan antara bunga tunggal dan majemuk berdasarkan nilai pokok, suku bunga, dan jangka waktu yang sama. Pengguna dapat memahami mana yang lebih menguntungkan untuk investasi mereka.</p>
-                <div class="mt-auto">
-                    <a href="/comparison-form" class="btn btn-primary">Go to Comparison</a>
+              </div>
+              <div class="flex flex-col gap-3 pb-3">
+                <div
+                  class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                  style='background-image: url("https://cdn.usegalileo.ai/sdxl10/e30352ca-adf1-4101-b885-ed74a2a7f9a6.png");'
+                ></div>
+            </a>
+            <a href="/compound-interest-form">
+                <div>
+                  <p class="text-[#1C160C] text-base font-medium leading-normal">MENGHITUNG BUNGA MAJEMUK</p>
+                  <p class="text-[#A18249] text-sm font-normal leading-normal">Muhamad Husni Mubarok</p>
                 </div>
+              </div>
+            </a>
+            <a href="/annuity-form">
+              <div class="flex flex-col gap-3 pb-3">
+                <div
+                  class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                  style='background-image: url("https://cdn.usegalileo.ai/sdxl10/53aa4aec-7540-4c1f-acf4-7ee17f684b0e.png");'
+                ></div>
+                <div>
+                  <p class="text-[#1C160C] text-base font-medium leading-normal">MENGHITUNG ANUITAS</p>
+                  <p class="text-[#A18249] text-sm font-normal leading-normal">Muhamad Husni Mubarok</p>
+                </div>
+              </div>
+            </a>
+            <a href="/comparison-form">
+              <div class="flex flex-col gap-3 pb-3">
+                <div
+                  class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+                  style='background-image: url("https://cdn.usegalileo.ai/sdxl10/ec118d59-dd0d-43be-ad1d-866087dfd972.png");'
+                ></div>
+                <div>
+                  <p class="text-[#1C160C] text-base font-medium leading-normal">MEMBANDINGKAN BUNGA</p>
+                  <p class="text-[#A18249] text-sm font-normal leading-normal">Muhamad Husni Mubarok</p>
+                </div>
+            </a>
+              </div>
             </div>
+          </div>
         </div>
+        <footer class="flex justify-center" style="background-color:#FFCF9D" border-radius="20rem";>
+          <div class="flex max-w-[960px] flex-1 flex-col">
+            <footer class="flex flex-col gap-6 px-5 py-10 text-center @container">
+              <p class="text-[#A18249] text-base font-normal leading-normal" > Copyright © 2023 FINANSKU</p>
+            </footer>
+          </div>
+        </footer>
+      </div>
     </div>
-</div>
-@endsection
+  </body>
+</html>
